e5cc69b9a758f09e39f9fcda807e9584d4722e2f
Dmitry Kolganov <dmk78@inbox.ru>
2019-03-24 23:17:45 +0500

    совместил загрузку урл и имени в одном цикле

--- app/src/main/java/com/android/dmk78/guessthestar/MainActivity.java
+++ app/src/main/java/com/android/dmk78/guessthestar/MainActivity.java
@@ -1,10 +1,12 @@
 package com.android.dmk78.guessthestar;
 
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Star> arrayListStars;
     private String myUrl = "http://www.posh24.se/kandisar";
     private String resultUrl;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // скачиваем веб страницу целиком
         DownloadTask task = new DownloadTask();
@@ -40,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // вытаскиваем имена и адреса картинок
-        Pattern patternImg = Pattern.compile("src=\"(.*?)\"");
+        Pattern patternImg = Pattern.compile("img src=\"(.*?)\" alt=");
         Pattern patternName = Pattern.compile("alt=\"(.*?)\"");
 
         Matcher matcherImg = patternImg.matcher(resultUrl);
@@ -48,23 +52,35 @@ public class MainActivity extends AppCompatActivity {
         String url;
         String name;
         int indexJava; //нахождение подстроки в строке
+
         while (matcherImg.find()) {
 
-            //Log.i("Myname", matcherImg.group(1));
+            matcherName.find();
+            name = matcherName.group(1);
             url = matcherImg.group(1);
-            indexJava = url.indexOf("http://cdn.posh24.se/images/");
+            Log.i("Myname", name);
+            indexJava = url.indexOf("http://cdn.posh24.se/images/:profile/");
 
             if (indexJava != -1) {
-                Log.i("Myname", matcherImg.group(1));
+                Log.i("Myname", url);
+
+
 
             }
 
+
         }
+
+
+
+      /*  count=0;
         while (matcherName.find()) {
             Log.i("Myname", matcherName.group(1));
             name = matcherName.group(1);
+            count++;
         }
 
+        Log.i("Myname", String.valueOf(count));*/
 
     }
 
